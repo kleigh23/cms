471b20998046f227f43bc3d0d4ebaf63a560aa31
kleigh23 <rob22003@byui.edu>
2023-01-14 17:15:03 -0700

    finished cms project

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent {
+  contacts: Contact[] = [
+    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg'), 
+    new Contact('2', 'Rex Barzeer', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg')
+  ]
+
+}
